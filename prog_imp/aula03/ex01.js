d80e25b8fba11743901a0e70f713b4f3abd6a3ea
caiocastrofrs <caiocastrofrs@gmail.com>
2021-05-31 15:47:45 -0300

    adicionando ex01 na pasta aula03

--- prog_imp/aula03/ex01.js
+++ prog_imp/aula03/ex01.js
@@ -7,4 +7,17 @@ var peso = 75;
 var temFaculdade = false;
 var ehFeio = false;
 
-console.log(nome);
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof peso);
+console.log(typeof temFaculdade);
+console.log(typeof ehFeio);
+
+function alteraIdade(idade)
+{
+    idade++;
+    console.log(idade);
+}
+
+alteraIdade(idade);
+
