bcc496ede010fad5fbc0731342d57ff91bda33f7
caiocastrofrs <caiocastrofrs@gmail.com>
2021-06-02 14:25:51 -0300

    adicionando exercicio if de status

---
+++ prog_imp/exercicios/ex_if_status.js
@@ -0,0 +1,12 @@
+var status = false;
+
+if (status == true)
+{
+    console.log("O valor é true - verdadeiro")
+}else if (status == false)
+{
+    console.log("O valor é false - falso")
+}
+else {
+    console.log("variavel sem valor algum")
+}
