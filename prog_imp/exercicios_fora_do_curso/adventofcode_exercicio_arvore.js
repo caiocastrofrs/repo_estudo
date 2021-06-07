dd8b268754334efa8a008488083358c5525c43b2
caiocastrofrs <caiocastrofrs@gmail.com>
2021-06-07 08:48:10 -0300

    adicionando exercicio arvore atualizado MAS não terminado

--- prog_imp/exercicios_fora_do_curso/adventofcode_exercicio_arvore.js
+++ prog_imp/exercicios_fora_do_curso/adventofcode_exercicio_arvore.js
@@ -325,9 +325,23 @@ var caminho =
     ,".#...#.........#.#.##.#........"
     ]
 
-let posicao_prox_array = 0
-for (let i = 0; i <= caminho.length; i++)
+let posicao_array = 0
+let contador_arvore = 0;
+for (let i = 0; i <= caminho.length-1; i++)
 {
-    
+    var tem_arvore = caminho[i].slice(posicao_array+1, posicao_array+4)
+    for (let i_arvore = 0; i_arvore <= tem_arvore.length; i_arvore++)
+    {
+        if (tem_arvore.slice(i_arvore, i_arvore+1) == '#')
+        {
+            contador_arvore++
+        }
+    }
+    posicao_array+=3
+    if (posicao_array > 27)
+    {
+        posicao_array = 0
+    }
+    console.log("elemento array: " + caminho[i] + " posicao do array: " + posicao_array + " tem_arvore : " + tem_arvore)
 }
 
