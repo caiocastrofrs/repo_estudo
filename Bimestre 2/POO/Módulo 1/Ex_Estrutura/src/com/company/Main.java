709d1844839bd7a8225c1dc34af60df8fa47228c
caiocastrofrs <caiocastrofrs@gmail.com>
2021-08-11 02:07:35 -0300

    subindo alguns exercicios do playground

---
+++ Bimestre 2/POO/Módulo 1/Ex_Estrutura/src/com/company/Main.java
@@ -0,0 +1,113 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        int number1 = 1;
+//        int number2 = 2;
+//        if (number1%number2 == 0) {
+//            System.out.println("Yes");
+//        }else{
+//            System.out.print("No!");
+//
+//        }
+//        Integer value1 = 10;
+//        Integer value2 = 30;
+//        int comparar;
+//
+//        if(value1.equals(value2)){
+//            System.out.println("São iguais!");
+//        }else {
+//            comparar = value2.compareTo(value1);
+//            System.out.println("comparar: " + comparar);
+//            if(comparar > 0) {
+//                System.out.println("value1 é maior que value2");
+//            }else {
+//                System.out.println("value2 é maior que value1");
+//            }
+//        }
+//        String textOne = "Caio";
+//        String textTwo = new String("caio");
+//
+//        if(textOne.equals(textTwo)) {
+//            System.out.println("São iguais!");
+//        }else {
+//            System.out.println("Não são iguais!");
+//        }
+//        Integer value03 = 3;
+//        Integer value04 = 4;
+//        int comparar;
+//        if(value03.equals(value04)) {
+//            System.out.println("São iguais!");
+//        }else {
+//            System.out.println("Não são iguais!");
+//        }
+//        comparar = value03.compareTo(value04);
+//        if (comparar > 0) {
+//            System.out.println("value03 é maior!");
+//        }
+//        else {
+//            System.out.println("value04 é maior!");
+//        }
+//        Scanner scanner;
+//        scanner = new Scanner(System.in);
+//        int number1;
+//        int number2;
+//        float coeficiente;
+//        String nome;
+//        char inicial;
+//        System.out.println("Digite um numero:");
+//        number1 = scanner.nextInt();
+//        System.out.println("Digite outro numero:");
+//        number2 = scanner.nextInt();
+//        System.out.println("Digite o coeficiente:");
+//        coeficiente = scanner.nextFloat();
+//
+//        System.out.println("Digite seu nome:");
+//        nome = scanner.nextLine();
+//        inicial = nome.charAt(0);
+//        System.out.println(inicial);
+//        String nome = "Caio";
+//        String sobrenome = "Castro";
+//
+//        char inicialNome = nome.charAt(0);
+//        char inicialSobrenome = sobrenome.charAt(0);
+//
+//        System.out.println("nome:" + nome + " " + sobrenome + "\nIniciais: " + inicialNome + " " + inicialSobrenome);
+//        int soma(int number1, number2) {
+//            return number1 + number2;
+//        }
+//        void mostrarMensagem(String mensagem) {
+//            System.out.println(mensagem);
+//        }
+//            boolean eDivisivel(int num, divisor) {
+//                if (n1%n2 == 0) {
+//                    return true;
+//                }else {
+//                    return false;
+//                }
+//            }
+//        switch(variavel) {
+//            case valor1:
+//                    //codigo
+//                    break;
+//            case valor2:
+//                    //codigo
+//                    break;
+//            case valor3:
+//                    //codigo
+//                    break;
+//        }
+//        for(Integer i = 0; i<valorMaximo; i++) {
+//            //código
+//        }
+//        for(Object object:listaDeObjetos) {
+//            //código
+//        }
+//        while(condição) {
+//            //código
+//        }
+    }
+}
