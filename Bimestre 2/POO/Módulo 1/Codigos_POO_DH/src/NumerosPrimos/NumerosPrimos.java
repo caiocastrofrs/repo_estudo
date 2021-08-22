287e297b73e169b08947548501b81c4f5691a1e4
caiocastrofrs <caiocastrofrs@gmail.com>
2021-08-22 17:03:32 -0300

    subindo exercicios de POO

--- Bimestre 2/POO/Módulo 1/Codigos_POO_DH/src/NumerosPrimos/NumerosPrimos.java
+++ Bimestre 2/POO/Módulo 1/Codigos_POO_DH/src/NumerosPrimos/NumerosPrimos.java
@@ -3,6 +3,22 @@ import java.util.Scanner;
 
 public class NumerosPrimos {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um número e retornaremos os primeiros n numeros.");
+        int numeroUsuario = scanner.nextInt();
+
+        for(int i = 2;i <= numeroUsuario; ++i) {
+            boolean primo = true;
+
+            for(int j = 2; j < i; ++j) {
+                if(i%j == 0){
+                    primo = false;
+                }
+            }
+            if(primo) {
+                System.out.println(i);
+            }
+        }
 
     }
 }
