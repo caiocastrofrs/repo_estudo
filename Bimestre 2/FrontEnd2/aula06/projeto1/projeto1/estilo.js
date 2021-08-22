10e73f740b9e815a4790924396f1b84e9bf0d6e5
caiocastrofrs <caiocastrofrs@gmail.com>
2021-08-22 17:00:40 -0300

    subindo projeto criando um piano usando DOM da rocketseat

---
+++ Bimestre 2/FrontEnd2/aula06/projeto1/projeto1/estilo.js
@@ -0,0 +1,10 @@
+/* const botaoTitulo = document.querySelector('.btnMudarTitulo');
+
+document.addEventListener("click", function(){
+  let titulo = document.querySelector('.titulo');
+  titulo.style.color = 'black';
+  titulo.style.textAlign = 'center';
+  titulo.style.fontSize = '22px';
+  titulo.style.backgroundColor = '#fff'; 
+})
+ */
