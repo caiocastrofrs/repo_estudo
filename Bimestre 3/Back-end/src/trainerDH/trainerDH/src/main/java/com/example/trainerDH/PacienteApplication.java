6c7b767e2e9c1090f39f3a99d9679c1728da1d95
caiocastrofrs <caiocastrofrs@gmail.com>
2021-11-09 10:14:43 -0300

    Aula 18 Mesa de Trabalho

---
+++ Bimestre 3/Back-end/src/trainerDH/trainerDH/src/main/java/com/example/trainerDH/PacienteApplication.java
@@ -0,0 +1,12 @@
+package com.example.trainerDH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacienteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PacienteApplication.class, args);
+    }
+}
